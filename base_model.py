364538ab6160d91760892935976a7e601ef8c929
Mahao05 <prudencemolefe93@gmail.com>
2024-02-13 13:20:41 +0000

    AirBnB_clone

--- base_model.py
+++ base_model.py
@@ -20,3 +20,28 @@ class BaseModel:
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
         return dict_repr
+
+class BaseModel:
+    def __init__(self, id=None, created_at=None):
+        self.id = id
+        self.created_at = created_at
+
+my_instance = BaseModel(id="123", created_at="2024-02-13T14:30:00")
+
+my_dict = my_instance.to_dict()
+print(my_dict)
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                    if key in ["created_at", "updated_at"]:
+                        pass
+        else:
+            pass
+
+recreated_instance = BaseModel(**my_dict)
+print(recreated_instance.id)  
+print(recreated_instance.created_at)  
