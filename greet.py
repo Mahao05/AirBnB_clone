5696c3789a8701f4599232ed8fce881225776d5c
Mahao05 <prudencemolefe93@gmail.com>
2024-03-26 17:09:36 -0700

    Airbnb

--- greet.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def greet(name):
-    
-    if not isinstance(name, str):
-        raise TypeError('Name must be a string.')
-    
-    greeting = f'Hello, {name}! How are you today?'
-    return greeting
-
--- user.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-class User(BaseModel):
-    email: str = ''
-    password: str = ''
-    first_name: str = ''
-    last_name: str = ''
